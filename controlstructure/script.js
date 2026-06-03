a55ac19b1ab4d79e500564b642904418aecfb24b
Psdannb <psdannb@gmail.com>
2026-06-03 08:04:59 +0545

    day 1

---
+++ controlstructure/script.js
@@ -0,0 +1,72 @@
+let age = 20;
+// console.log(age);
+// if....else if
+
+/*if (age >= 18) {
+  console.log("You are an adult.");
+} else if (age >= 13) {
+  console.log("You are a minor.");
+} else if (age >= 0) {
+  console.log("You are a child.");
+} else {
+  console.log("Invalid age.");
+}
+*/
+
+// switch statement
+/*let day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("It's Monday.");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday.");
+    break;
+  // Add more cases as needed
+  default:
+    console.log("Invalid day.");
+}
+*/
+
+// for loop
+/*for (initialization; condition; increment/decrement) {
+  console.log("Iteration: " + i);
+}
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= i; j++) {
+    document.write("*");
+    // document.write(" ");
+  }
+  document.write("<br>");
+}
+*/
+
+/* while loop 
+while(variable condition) {
+    block of code to be executed
+}
+
+let x = -20;
+while (x >= 0) {
+while (x >= 0) {
+  console.log(x);
+  x--;
+}*/
+
+/* do...while loop
+do {
+  block of code to be executed
+} while (condition);
+
+ let x = 10;
+ do {
+   console.log(x);
+   x--;
+ } while (x >= 0);
+   */
+/* sanam question example
+let name = "dan bahadur adhikari";
+let eg = `my name is ${name}`;
+console.log(eg);
+*/
