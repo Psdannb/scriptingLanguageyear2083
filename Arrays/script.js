bd4ee6a2181102a92f35b39808335a59f05c3ccd
Psdannb <psdannb@gmail.com>
2026-06-08 07:52:38 +0545

    array methods

--- Arrays/script.js
+++ Arrays/script.js
@@ -60,7 +60,7 @@ let bca = [];
 // console.log(bca);
 
 // how to remove elem from an array
-let college = ["BCA", "BBA", "BCom", "BSc"];
+// let college = ["BCA", "BBA", "BCom", "BSc"];
 // let poppedElem = college.pop();
 // console.log(poppedElem);
 // console.log(college);
@@ -68,6 +68,51 @@ let college = ["BCA", "BBA", "BCom", "BSc"];
 // let shiftedElem = college.shift();
 // console.log(shiftedElem);
 // console.log(college);
-let slicedArray = college.slice(1, 4);
-console.log(slicedArray);
-console.log(college);
+
+// let slicedArray = college.slice(1, 4);
+// console.log(slicedArray);
+// console.log(college);
+
+// let splicedArray = college.splice(1, 2, "BTech", "MCA", "MBA");
+// console.log(splicedArray);
+// console.log(college);
+
+// how to find the index of an element in an array
+let college = ["BCA", "BBA", "BCom", "BSc", "BCom", "BbCom"];
+// let indexOfBCom = college.indexOf("BCom");
+// console.log(indexOfBCom);
+
+// let indexOfBCom = college.lastIndexOf("BCom");
+// let indexOfBCom = college.lastIndexOf("BCCA");
+// console.log(indexOfBCom);
+
+// include() method
+// let isBCAIncluded = college.includes("BnCA");
+// console.log(isBCAIncluded);
+
+// find() method
+let studentsData = [
+  { name: "Nihareeka", age: 20, city: "Bangalore" },
+  { name: "Satyarth", age: 21, city: "Delhi" },
+  { name: "Shivam", age: 22, city: "Mumbai" },
+  { name: "Rohit", age: 23, city: "Chennai" },
+];
+// let student = studentsData.find((student) => student.name === "Shivam");
+// console.log(student);
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let evenNumber = numbers.find((number) => number % 2 === 0);
+// console.log(evenNumber);
+// let evenNumbers = numbers.filter((number) => number % 2 === 0);
+// console.log(evenNumbers);
+
+let sumofarray = numbers.reduce((sum, currentValue) => {
+  return sum + currentValue;
+});
+// console.log(sumofarray);
+// array concatination
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let concatenatedArray = array1.concat(array2, array1, [`a`, `b`, `c`]);
+// console.log(concatenatedArray);
+// console.log(array1);
+// console.log(array2);
