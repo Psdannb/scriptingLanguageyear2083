9330fe5eb707fc8dbe4e8aa3419081afd804efff
Psdannb <psdannb@gmail.com>
2026-06-30 07:51:29 +0545

    event handling updated

--- events/script.js
+++ events/script.js
@@ -3,13 +3,54 @@ function clicked() {
 }
 
 let countbtn = document.querySelector("#countbtn");
-
+let removecountbtn = document.getElementById("removeEvent");
 // console.log(countbtn);
 // countbtn.addEventListener("click", clicked);
+
+countbtn.addEventListener("click", countclick);
+removecountbtn.addEventListener("click", () => {
+  countbtn.removeEventListener("click", countclick);
+});
 let clickValue = 0;
-countbtn.addEventListener("click", () => {
+function countclick() {
   clickValue++;
   let msg = `The button has been clicked ${clickValue} times`;
   //   alert(msg);
   console.log(msg);
+}
+// document.addEventListener("scroll", () => {
+//   console.log("scroll detected");
+// });
+// document.addEventListener("DOMContentLoaded", () => {
+//   alert("Window content loaded sucessfully");
+// });
+
+// document.addEventListener("mousemove", () => {
+//   console.log("Mouse moved");
+// });
+document.addEventListener("keypress", (event) => {
+  // console.log("Keyboard btn is pressed");
+  // console.log(event);
+});
+
+let mouseeventbtn = document.getElementById("mouseevent");
+// mouseeventbtn.addEventListener("mouseover", mouseventhandler);
+// mouseeventbtn.addEventListener("mouseout", mouseouteventhandler);
+// mouseeventbtn.addEventListener("dblclick", dblclickhandler);
+
+function mouseventhandler() {
+  console.log("mouse over event is handled");
+}
+
+function mouseouteventhandler() {
+  console.log("mouse out event is handled");
+}
+function dblclickhandler() {
+  console.log("double clicked");
+}
+
+let ytlink = document.getElementById("ytlink");
+ytlink.addEventListener("click", (Event) => {
+  // console.log(Event);
+  Event.preventDefault();
 });
