535b0b7beb51b0de22db4b05991ee1eda7939e0f
Psdannb <psdannb@gmail.com>
2026-06-30 20:43:51 +0545

    Forms theory updated

---
+++ forms/script.js
@@ -0,0 +1,34 @@
+/*
+Here we are going to discuss how we can access the form elements values/contents and manipulate them.
+We will also discuss about how to use them in the js and for what purposes.
+1.textfield,textarea and hidden fields 
+how to get values and set values?
+key point: .value;
+example:
+*/
+
+/*
+2.select boxes
+how to get and set selectedIndex ?
+key point: .selectedIndex, .value
+Example:
+*/
+
+/*
+3.Checkboxes
+how to get checked value and set ?
+key point : .checked
+Example:
+*/
+
+/*
+4.radiobuttons
+how to get and set selectedIndex?
+key point : .checked,.value
+Example:
+*/
+
+/*
+5.submit button
+optional here as we have already discussed these things on event handling but lets have a look on this simple example
+*/
