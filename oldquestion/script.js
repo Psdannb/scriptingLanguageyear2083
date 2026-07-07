2414ce5acc88729a6a46af043605b4fc1e8cf7f9
Psdannb <psdannb@gmail.com>
2026-07-07 08:01:19 +0545

    old question solved

--- oldquestion/script.js
+++ oldquestion/script.js
@@ -1,5 +1,5 @@
 /* 
-2025 Qn : 2
+2025 Qn : 2 (2022 batch)
 Write a javascript program to accept n integer into an array calculate and display the sum and average of the numbers also find the largest number among them. 
 
 
@@ -19,7 +19,7 @@ console.log("The average of the numbers is: " + average);
 
 /*   2021 batch : Qn 2
 Write a js program to highlight all of the words over eight characters long  in the paragraph text (with red background).
-*/
+
 
 let paragraph = document.getElementById("text");
 let paragraphText = paragraph.innerText;
@@ -37,3 +37,37 @@ pArray.forEach((word) => {
   }
 });
 paragraph.innerHTML = newParagraph;
+*/
+
+/*
+2020 Batch Qn:2 (5 marks)
+Write a HTML Code to design a form with three radio button green, red, and blue. Write a Javascript code that will change the background color of the page when user clicks on particular button .
+*/
+function changeColor(color) {
+  let body = document.getElementById("body");
+  body.style.backgroundColor = color;
+}
+/*
+let greenbtn = document.getElementById("green");
+let redbtn = document.getElementById("red");
+let bluebtn = document.getElementById("blue");
+
+greenbtn.addEventListener("click", () => {
+  changeColor("green");
+});
+redbtn.addEventListener("click", () => {
+  changeColor("red");
+});
+bluebtn.addEventListener("click", () => {
+  changeColor("blue");
+});
+*/
+let bgbutton = document.getElementsByName("bgbutton");
+// console.log(bgbutton);
+bgbutton.forEach((element) => {
+  // console.log(element);
+  element.addEventListener("click", () => {
+    let elemValue = element.value;
+    changeColor(elemValue);
+  });
+});
