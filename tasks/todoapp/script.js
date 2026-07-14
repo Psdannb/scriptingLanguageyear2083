c2f06e610399a62dc0f58029d01c6cebe688f2ba
Psdannb <psdannb@gmail.com>
2026-07-14 07:55:07 +0545

    todo app completed

--- tasks/todoapp/script.js
+++ tasks/todoapp/script.js
@@ -8,14 +8,16 @@ entryform.addEventListener("submit", (event) => {
   let description = document.getElementById("description");
   let descriptionValue = description.value.trim();
   //   console.log(descriptionValue);
-  addTodo(titleValue, descriptionValue);
+  let status = "pending";
+  addTodo(titleValue, descriptionValue, status);
   title.value = "";
   description.value = "";
 });
-function addTodo(title, description) {
+function addTodo(title, description, status) {
   let oldData = getTodo();
-  oldData.push({ title, description });
+  oldData.push({ title, description, status });
   saveTodo(oldData);
+  renderTodo();
 }
 function saveTodo(todos) {
   localStorage.setItem("todoappData", JSON.stringify(todos));
@@ -32,6 +34,7 @@ function renderTodo() {
   console.log(oldData);
 
   let todostaskDiv = document.getElementById("todostask");
+  todostaskDiv.innerHTML = "";
   oldData.forEach((data) => {
     // console.log(data);
     let singleTodoDiv = document.createElement("div");
@@ -41,12 +44,78 @@ function renderTodo() {
     heading.setAttribute("class", "todoTitle");
     heading.innerText = data.title;
 
+    let description = document.createElement("P");
+    description.setAttribute("class", "todoDescription");
+    description.innerText = data.description;
+
+    let confirmbtn = document.createElement("button");
+    confirmbtn.setAttribute("class", "confirmBtn");
+    confirmbtn.innerText = "Confirm";
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "deleteBtn");
+    deleteBtn.innerText = "Delete";
+
+    if (data.status !== "pending") {
+      heading.setAttribute("class", "done");
+      description.setAttribute("class", "done");
+      confirmbtn.style.display = "none";
+    }
+
     singleTodoDiv.append(heading);
+    singleTodoDiv.append(description);
+
+    singleTodoDiv.append(confirmbtn);
+
+    singleTodoDiv.append(deleteBtn);
     todostaskDiv.append(singleTodoDiv);
   });
+  activateDeleteBtn();
+  activateConfirmBtn();
 }
-renderTodo();
-function deleteTodo() {
-  localStorage.removeItem("todoappData");
+
+function activateDeleteBtn() {
+  let deleteBtns = document.querySelectorAll(".deleteBtn");
+  // console.log(deleteBtns);
+
+  deleteBtns.forEach((btn, index) => {
+    // console.log(index);
+    btn.addEventListener("click", () => {
+      // console.log(index);
+      deleteTodo(index);
+    });
+  });
 }
-// deleteTodo();
+
+function activateConfirmBtn() {
+  let confirmBtns = document.querySelectorAll(".confirmBtn");
+
+  confirmBtns.forEach((btn, index) => {
+    // console.log(index);
+    btn.addEventListener("click", () => {
+      // console.log(index);
+      confirmTodo(index);
+    });
+  });
+}
+
+function deleteTodo(index) {
+  let oldData = getTodo();
+  // console.log(oldData[index]);
+  let flag = confirm("DO you really want to delete?");
+  if (flag) {
+    oldData.splice(index, 1);
+    saveTodo(oldData);
+    renderTodo();
+  }
+}
+
+function confirmTodo(index) {
+  let olddata = getTodo();
+  olddata[index].status = "done";
+  saveTodo(olddata);
+  renderTodo();
+}
+window.addEventListener("load", () => {
+  renderTodo();
+});
