82081b915639612ca605efef4df038042c1aa298
Psdannb <psdannb@gmail.com>
2026-07-16 20:44:55 +0545

    basic info about php

---
+++ PHPCode/basic.php
@@ -0,0 +1,41 @@
+<?php
+/*
+What is PHP ?
+PHP (Hypertext Preprocessor) is a server-side scripting language used to develop dynamic and interactive web applications. PHP code is embedded within HTML and is executed on the web server before the page is sent to the client's browser.
+A PHP script begins with the <?php opening tag and ends with the ?> closing tag.
+
+Basic Rules of PHP Syntax:
+1. Every PHP statement ends with a semicolon (;).
+2. Variable names are case-sensitive.
+
+Comments in PHP:
+Comments are non-executable statements used to explain the code, improve readability, and temporarily disable code
+during testing or debugging. The PHP interpreter ignores comments during execution.
+
+Types of Comments
+a) Single-Line Comment (// or #)
+b) Multi-Line Comment
+
+
+Variables in PHP:
+A variable is a named memory location used to store data that can be accessed and modified during program execution.
+In PHP, every variable begins with the $ (dollar) symbol.
+syntax: $variable_name = value;
+
+
+Rules for Naming Variables:
+1) A variable must begin with the $ symbol.
+2) The first character after $ must be a letter or an underscore (_).
+3) Variable names cannot start with a number.
+4) Variable names may contain letters, numbers, and underscores.
+5) Variable names are case-sensitive.
+
+PHP is loosely typed i.e. PHP automatically determines the data type of a variable based on the assigned value.
+
+Example:
+$name = "Ram"; // String
+// $age = 20; // Integer
+// $height = 5.8; // Float
+// $isStudent = true; // Boolean
+*/
+?>
