0f07167e3e132b66fec9b2bcf00115393f41ee77
Psdannb <psdannb@gmail.com>
2026-09-23 07:48:24 +0545

    old question solution

--- PHPCode/oldquestionsolutions.php
+++ PHPCode/oldquestionsolutions.php
@@ -50,7 +50,8 @@ for($i=8;$i>=1;$i--){
 /*
 
 QN: write a PHP function that accepts username and password as arguments and check with student table, if credential match, redirect to dashboard page otherwise display 'invalid username/password'.
-*/
+Solution:
+
 
 function validateuser($submittedusername,$Submittedpassword){
 $connectionstring=mysqli_connect("localhost","root","","scriptinglanguage");
@@ -78,3 +79,55 @@ else{
 }
 }
  validateuser("ram","jksfgkasd");
+
+ */
+/*
+Question:
+Implement an interface in PHP with specific properties (radius for a circle class, side for a square class, and base and height for a triangle class) to ensure that multiple classes define their own implementation of a method (calculate Area) utilizing these properties in their calculations. [10 marks]
+Solution:
+*/
+interface masterinfo{
+    public function calculateArea();
+}
+class circle implements masterinfo{
+public $radius;
+public function __construct($r){
+    $this->radius=$r;
+}
+public function calculateArea(){
+    $area= 3.14*$this->radius*$this->radius;
+    echo "The area of a circle = $area";
+    echo "<br>";
+}
+
+}
+class square implements masterinfo{
+    public $side;
+    public function __construct($s){
+        $this->side=$s;
+    }
+  public function calculateArea(){
+      $area= $this->side*$this->side;
+    echo "The area of a square= $area";
+       echo "<br>";
+  }
+}
+class triangle implements masterinfo{
+    public $base;
+    public $height;
+    public function __construct($b,$h){
+        $this->base=$b;
+        $this->height=$h;
+    }
+    public function calculateArea(){
+        $area= (1/2)*$this->base*$this->height;
+        echo "The area of a triangle= $area";
+           echo "<br>";
+    }
+}
+$circle= new circle(4);
+$circle->calculateArea();
+$square= new square(5);
+$square->calculateArea();
+$triangle= new triangle(2,4);
+$triangle->calculateArea();
